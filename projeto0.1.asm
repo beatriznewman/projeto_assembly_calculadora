43bcd3231562be33915dd835514a7a14e6fe97cd
lubaptista <111128530+lubaptista@users.noreply.github.com>
2022-11-06 18:29:22 -0300

    Add files via upload

--- projeto0.1.asm
+++ projeto0.1.asm
@@ -30,9 +30,9 @@ repetir DB 10, " Realizar outra operacao? (Digite s para sim)" , '$'
     LEA DX, fecha              ; coloca o endereco da mensagem 'fecha' no registrador DX
     INT 21H                    ; executa funcao, imprimindo o conteudo do endereco DX                    
 
-    CALL pula                  ;chama procedimento 'pula'(), para pular a linha
+    inicio:
+      CALL pula                ; chama procedimento 'pula', para pular a linha
 
-   inicializa:
       MOV AH, 09               ; funcao para impressao de string 
       LEA DX, escolha          ; coloca o endereco da mensagem 'escolha' no registrador DX
       INT 21H                  ; executa funcao, imprimindo o conteudo do endereco DX               
@@ -41,76 +41,83 @@ repetir DB 10, " Realizar outra operacao? (Digite s para sim)" , '$'
       INT 21H                  ; executa funcao, guardando o caractere inserido em AL
 
       CMP AL, "+"              ; comparar conteúdo de AL(sinal da operacao a ser realizada) com '+'
-      JE  verifica             ; saltar se igual a zero para 'verifica'(53)
+      JE  verifica             ; saltar se igual a zero para 'verifica'
       CMP AL, "-"              ; comparar conteúdo de AL(sinal da operacao a ser realizada) com '-'
-      JE verifica              ; saltar se igual a zero para 'verifica'(53)
+      JE verifica              ; saltar se igual a zero para 'verifica'
       CMP AL, "*"              ; comparar conteúdo de AL(sinal da operacao a ser realizada) com '*'
-      JE verifica              ; saltar se igual a zero para 'verifica'(53)
+      JE verifica              ; saltar se igual a zero para 'verifica'
       CMP AL, "/"              ; comparar conteúdo de AL(sinal da operacao a ser realizada) com '/'
-      JE verifica              ; saltar se igual a zero para 'verifica'(53)
+      JE verifica              ; saltar se igual a zero para 'verifica'
       
-      JMP erro                 ; se a entrada não for as possíveis operações, dará erro(103)
+      JMP erro                 ; se a entrada não for as possíveis operações, dará 'erro'
  
-   verifica:
-     MOV CH, AL
+    verifica:
+      MOV CH, AL               ; colocar o conteudo de AL(operacao da conta a ser executada) em CH 
 
-     MOV AH, 09                ; funcao para impressao de string 
-     LEA DX, numero1           ; coloca o endereco da mensagem 'numero1' no registrador DX
-     INT 21H                   ; executa funcao, imprimindo o conteudo do endereco DX         
+      MOV AH, 09               ; funcao para impressao de string 
+      LEA DX, numero1          ; coloca o endereco da mensagem 'numero1' no registrador DX
+      INT 21H                  ; executa funcao, imprimindo o conteudo do endereco DX         
 
-     MOV AH, 01                ; funçao de leitura de caractere
-     INT 21H                   ; executa funcao, guardando o caractere inserido em AL                  
-     MOV BH, AL                ; colocar o conteudo de AL(caractere inserido) em BH 
-     SUB BH, 30h               ; subtrai 30h de BH(codigo ascii do numero inserido)
+      MOV AH, 01               ; funçao de leitura de caractere
+      INT 21H                  ; executa funcao, guardando o caractere inserido em AL                  
+      MOV BH, AL               ; colocar o conteudo de AL(caractere inserido) em BH 
+      SUB BH, 30h              ; subtrai 30h de BH(codigo ascii do numero inserido)
      
-     MOV AH, 09                ; funcao para impressao de string 
-     LEA DX, numero2           ; coloca o endereco da mensagem 'numero2' no registrador DX
-     INT 21H                   ; executa funcao, imprimindo o conteudo do endereco DX           
+      MOV AH, 09               ; funcao para impressao de string 
+      LEA DX, numero2          ; coloca o endereco da mensagem 'numero2' no registrador DX
+      INT 21H                  ; executa funcao, imprimindo o conteudo do endereco DX           
 
-     MOV AH, 01                ; funçao de leitura de caractere
-     INT 21H                   ; executa funcao, guardando o caractere inserido em AL                 
-     MOV BL, AL                ; colocar o conteudo de AL(caractere inserido) em BH 
-     SUB BL, 30h               ; subtrair 30h de BL(codigo ascii do numero inserido)
+      MOV AH, 01               ; funçao de leitura de caractere
+      INT 21H                  ; executa funcao, guardando o caractere inserido em AL                 
+      MOV BL, AL               ; colocar o conteudo de AL(caractere inserido) em BH 
+      SUB BL, 30h              ; subtrair 30h de BL(codigo ascii do numero inserido)
 
-   CMP CH,"+"                  ; compara o sinal '+' com o registardor CH, que contem a operacao a ser realizada 
-   JNZ nao_adic                ; pular para a proxima comparacao (->nao_adic(78)) caso nao for o sinal de '+'
-   CALL adicionar              ; chama o procedimento de soma -> adicionar(125)
+    CMP CH,"+"                 ; compara o sinal '+' com o registardor CH, que contem a operacao a ser realizada 
+    JNZ nao_adic               ; pular para a proxima comparacao (->nao_adic) caso nao for o sinal de '+'
+    CALL adicionar             ; chama o procedimento de soma -> adicionar
      
-   nao_adic: 
-     CMP CH,"-"                ; compara o sinal '-' com o registardor CH, que contem a operacao a ser realizada 
-     JNZ nao_sub               ; pular para a proxima comparacao (->nao_sub(83)) caso nao for o sinal de '-'
-     CALL subtrair             ; chama o procedimento de subtracao -> subtrair(133)
+    nao_adic: 
+      CMP CH,"-"               ; compara o sinal '-' com o registardor CH, que contem a operacao a ser realizada 
+      JNZ nao_sub              ; pular para a proxima comparacao (->nao_sub) caso nao for o sinal de '-'
+      CALL subtrair            ; chama o procedimento de subtracao -> 'subtrair'
 
-   nao_sub: 
-     CMP CH,"*"                ; comparar o sinal '*' com o registardor CH, que contem a operacao a ser realizada 
-     JNZ nao_mult              ; pular para a proxima comparacao (->nao_mult(89)) caso nao for o sinal de '*'
-     CALL multiplicar          ; chama o procedimento de multiplicacao -> multiplica()
+    nao_sub: 
+      CMP CH,"*"               ; comparar o sinal '*' com o registardor CH, que contem a operacao a ser realizada 
+      JNZ nao_mult             ; pular para a proxima comparacao (->nao_mult) caso nao for o sinal de '*'
+      CALL multiplicar         ; chama o procedimento de multiplicacao -> 'multiplicar'
 
-   nao_mult: 
-     ;CMP CH,"/"                ; comparae o sinal '/' com o registardor CH, que contem a operacao a ser realizada 
-     ;JNZ nao_div               ; pular para a proxima comparacao (->nao_div(94)) caso nao for o sinal de '/'
-     ;CALL divisao              ; chama o procedimento de divisao -> divisao()
+    nao_mult: 
+      CMP CH,"/"               ; compara o sinal '/' com o registardor CH, que contem a operacao a ser realizada 
+      JNZ nao_div              ; pular para a proxima comparacao (->nao_div) caso nao for o sinal de '/'
+      CALL dividir             ; chama oprocedimento de divisao -> 'dividir'
   
-   ;nao_div: 
+    nao_div: 
+      CALL pula                ; chama procedimento 'pula', para pular a linha      
+      CALL imprimir            ; chama procedimento 'imprimir', para impressao da conta
 
-     CALL pula                  ; chama procedimento 'pula'(), para pular a linha      
-     CALL imprimir1             ; chama procedimento 'imprimir1'(), para impressao da conta-> res. posit. 1 alg.
+      MOV AH, 09               ; funcao para impressao de string 
+      LEA DX, repetir          ; colocar o endereco da mensagem 'erro'no registrador DX
+      INT 21H                  ; executa funcao, imprimindo o conteudo do endereco DX         
 
-     JMP fim                    ; pula para o final do programa
+      MOV AH, 01               ; funçao de leitura de caractere
+      INT 21H                  ; executa funcao, imprimindo o conteudo do endereco DX         
 
+      CMP AL, 's'              ; compara o registrador AL com o s minusculo 
+      JE inicio                ; jump para o 'inicio', caso a pessoa escreva 's' para realizar outra operacao
+      CMP AL, 'S'              ; compara o registrador AL com o S maiusculo 
+      JE inicio                ; jump para o 'inicio', caso a pessoa escreva 'S' para realizar outra operacao 
 
+      JMP fim                  ; pula para o final do programa
 
-erro:
-MOV AH, 09                      ; funcao para impressao de string 
-lea DX, errado                  ; colocar o endereco da mensagem 'erro'no registrador DX
-INT 21h                         ; executa funcao, imprimindo o conteudo do endereco DX 
-JMP inicializa                  ; pular para o inicio -> inicializa(34)
+    erro:
+      MOV AH, 09               ; funcao para impressao de string 
+      LEA DX, errado           ; colocar o endereco da mensagem 'erro'no registrador DX
+      INT 21h                  ; executa funcao, imprimindo o conteudo do endereco DX 
+      JMP inicio               ; pular para o 'inicio'
 
-
-fim:
-MOV AH,4CH                     ; exit 
-INT 21H
-
+    fim:
+      MOV AH,4CH               ; exit 
+      INT 21H
 main ENDP
 
 
@@ -179,7 +186,16 @@ multiplicar PROC
       RET
 multiplicar ENDP
 
-imprimir1 PROC
+dividir PROC
+  POP SI
+
+  
+
+  PUSH SI
+  RET
+dividir ENDP
+
+imprimir PROC
    POP SI
 
    MOV AH, 02                  ; funcao para impressao de caractere
@@ -208,34 +224,26 @@ imprimir1 PROC
    MOV DL, DH                  ; mover para DL o simbolo '-' ou ' ', guardado em DH
    INT 21H                     ; executa funcao, imprimindo o conteudo de DL
 
-   CALL imprimir2              ; chama procedimento 'imprimir2', para imprimir o resultado: valor com 2 numeros
+    ;impressao resultado: numero com 2 digitos:
+
+   XOR CH, CH                  ; operador XOR entre CH e CH, zerando o registrador CH, para guardar resultado (XOR entre numeros iguais = 0)
+   MOV AX, CX                  ; mover para AX o conteudo de CL(resultado da conta)
+   MOV BL, 10                  ; mover para BL o numero 10
+   DIV BL                      ; dividir AX(registrador utilizado pela funcao), por 10(BL). Guarda o resultado em AL e o resto em AH
+   MOV CX, AX                  ; mover AX(resultado da conta) para CX, para evitar perda dos valores armazenados
+
+   MOV DL, CL                  ; mover para DL o resultado da divisao (CL: primeiro numero a ser impresso)
+   OR DL, 30h                  ; operador OR entre o conteudo de DL e o numero hexadecimal 30h, obtendo o valor decimal em DL
+   MOV AH, 02                  ; funcao para impressao de caractere
+   INT 21H                     ; executa funcao, imprimindo o conteudo de DL
+
+   MOV DL, CH                  ; mover para DL o resto da divisao (CH: segundo numero a ser impresso) 
+   OR DL, 30h                  ; operador OR entre o conteudo de DL e o numero hexadecimal 30h, obtendo o valor decimal em DL
+   MOV AH, 02                  ; funcao para impressao de caractere
+   INT 21H                     ; executa funcao, imprimindo o conteudo de DL
 
    PUSH SI
    RET
-imprimir1 ENDP
-
-imprimir2 PROC
-  POP SI
-
-  XOR CH, CH                  ; operador XOR entre CH e CH, zerando o registrador CH, para guardar resultado (XOR entre numeros iguais = 0)
-  MOV AX, CX                  ; mover para AX o conteudo de CL(resultado da conta)
-  MOV BL, 10                  ; mover para BL o numero 10
-  DIV BL                      ; dividir AX(registrador utilizado pela funcao), por 10(BL). Guarda o resultado em AL e o resto em AH
-  MOV CX, AX                  ; mover AX(resultado da conta) para CX, para evitar perda dos valores armazenados
-
-  MOV DL, CL                  ; mover para DL o resultado da divisao (CL: primeiro numero a ser impresso)
-  OR DL, 30h                  ; operador OR entre o conteudo de DL e o numero hexadecimal 30h, obtendo o valor decimal em DL
-  MOV AH, 02                  ; funcao para impressao de caractere
-  INT 21H                     ; executa funcao, imprimindo o conteudo de DL
-
-  MOV DL, CH                  ; mover para DL o resto da divisao (CH: segundo numero a ser impresso) 
-  OR DL, 30h                  ; operador OR entre o conteudo de DL e o numero hexadecimal 30h, obtendo o valor decimal em DL
-  MOV AH, 02                  ; funcao para impressao de caractere
-  INT 21H                     ; executa funcao, imprimindo o conteudo de DL
-
-  PUSH SI
-  RET
-imprimir2 ENDP
-
+imprimir ENDP
 
 end main
